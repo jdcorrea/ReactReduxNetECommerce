433d0695387d780701d8f719cce03f7623478f37
Juan David Correa Cano <sye.jdcorrea@gmail.com>
2023-09-19 12:25:33 -0500

    Adding links to the NavBar

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,4 +1,18 @@
-import { AppBar, Switch, Toolbar, Typography } from "@mui/material"
+import { AppBar, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material"
+import { NavLink } from 'react-router-dom'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import Badge from '@mui/material/Badge';
+
+const midLinks = [
+  {title: 'catalog', path: '/catalog'},
+  {title: 'about', path: '/about'},
+  {title: 'contact', path: '/contact'},
+]
+
+const rightLinks = [
+  {title: 'login', path: '/login'},
+  {title: 'register', path: '/register'},
+]
 
 interface Props {
   darkMode: boolean;
@@ -9,10 +23,44 @@ function Header({darkMode, handleThemeChange}: Props) {
   return (
     <AppBar position="static" sx={{mb: 4}}>
       <Toolbar>
-        <Typography variant="h6">
+        <Typography variant="h6" component={NavLink} 
+          to='/'
+          sx={{color: 'inherit', textDecoration: 'none'}}
+        >
           React Store
         </Typography>
         <Switch checked={darkMode} onChange={handleThemeChange}/>
+        <List sx={{display: 'flex'}}>
+          {midLinks.map(({title, path}) => (
+            <ListItem 
+              key={path} 
+              component={NavLink} 
+              to={path}
+              sx={{color: 'inherit', typography: 'h6'}}
+            >
+              {title.toUpperCase()}
+            </ListItem>
+          ))}
+        </List>
+
+        <IconButton size='large' edge='start' color='inherit' sx={{mr: 2}}>
+          <Badge badgeContent={4} color='secondary'>
+            <ShoppingCartIcon />
+          </Badge>
+        </IconButton>
+
+        <List sx={{display: 'flex'}}>
+          {rightLinks.map(({title, path}) => (
+            <ListItem 
+              key={path} 
+              component={NavLink} 
+              to={path}
+              sx={{color: 'inherit', typography: 'h6'}}
+            >
+              {title.toUpperCase()}
+            </ListItem>
+          ))}
+        </List>
       </Toolbar>
     </AppBar>
   )
