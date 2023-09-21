13ca211afd7d3a69ebda7fcc7bb6ef1ebfd061c6
Juan David Correa Cano <sye.jdcorrea@gmail.com>
2023-09-21 15:39:04 -0500

    Added validation errors

--- client/src/features/about/AboutPage.tsx
+++ client/src/features/about/AboutPage.tsx
@@ -3,9 +3,22 @@ import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
 import ButtonGroup from '@mui/material/ButtonGroup'
 import Button from '@mui/material/Button'
+import Alert from '@mui/material/Alert'
+import AlertTitle from '@mui/material/AlertTitle'
+import List from '@mui/material/List'
+import ListItemText from '@mui/material/ListItemText'
+import { useState } from 'react'
 
 
 function AboutPage() {
+  const [validationErrors, setValidationErrors] = useState<string[]>([])
+
+  function getValidationError() {
+    agent.TestErrors.getValidationError()
+      .then(() => console.log('should not see this'))
+      .catch(error => setValidationErrors(error))
+  }
+
   return (
     <Container>
       <Typography gutterBottom variant='h2'>
@@ -16,8 +29,21 @@ function AboutPage() {
         <Button variant='contained' onClick={() => agent.TestErrors.get401Error().catch(error => console.log(error))}>Test 401 Error</Button>
         <Button variant='contained' onClick={() => agent.TestErrors.get404Error().catch(error => console.log(error))}>Test 404 Error</Button>
         <Button variant='contained' onClick={() => agent.TestErrors.get500Error().catch(error => console.log(error))}>Test 500 Error</Button>
-        <Button variant='contained' onClick={() => agent.TestErrors.getValidationError().catch(error => console.log(error))}>Test Validation Error</Button>
+        <Button variant='contained' onClick={getValidationError}>Test Validation Error</Button>
       </ButtonGroup>
+      {
+        validationErrors.length > 0 &&
+          <Alert severity='error'>
+            <AlertTitle>Validation Errors</AlertTitle>
+            <List>
+              {
+                validationErrors.map((error, id) => (
+                  <ListItemText key={id}>{error}</ListItemText>
+                ))
+              }
+            </List>
+          </Alert>
+      }
     </Container>
   )
 }
