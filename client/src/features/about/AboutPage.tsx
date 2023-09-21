cf85b098e5f2e5113a72524ff8dfafe9205e1f28
Juan David Correa Cano <sye.jdcorrea@gmail.com>
2023-09-21 11:39:56 -0500

    Created react component to test error responses

--- client/src/features/about/AboutPage.tsx
+++ client/src/features/about/AboutPage.tsx
@@ -1,11 +1,24 @@
+import agent from '@app/api/agent'
+import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
+import ButtonGroup from '@mui/material/ButtonGroup'
+import Button from '@mui/material/Button'
 
 
 function AboutPage() {
   return (
-    <Typography variant="h2">
-      About page
-    </Typography>
+    <Container>
+      <Typography gutterBottom variant='h2'>
+        Errors for testing purposes
+      </Typography>
+      <ButtonGroup>
+        <Button variant='contained' onClick={() => agent.TestErrors.get400Error()}>Test 400 Error</Button>
+        <Button variant='contained' onClick={() => agent.TestErrors.get401Error()}>Test 401 Error</Button>
+        <Button variant='contained' onClick={() => agent.TestErrors.get404Error()}>Test 404 Error</Button>
+        <Button variant='contained' onClick={() => agent.TestErrors.get500Error()}>Test 500 Error</Button>
+        <Button variant='contained' onClick={() => agent.TestErrors.getValidationError()}>Test Validation Error</Button>
+      </ButtonGroup>
+    </Container>
   )
 }
 
--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -19,7 +19,7 @@ function ProductDetails() {
   useEffect(() => {
     id && agent.Catalog.details(parseInt(id))
       .then(response => setProduct(response))
-      .catch(error => console.log(error))
+      .catch(error => console.log(error.response))
       .finally(() => setLoading(false))
   }, [id])
   
