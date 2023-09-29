b55b13008ce22f49b903c46f3c493a438ee486e1
Juan David Correa Cano <sye.jdcorrea@gmail.com>
2023-09-29 11:00:29 -0500

    Entity Framework EF relations created

--- API/Entities/BasketItem.cs
+++ API/Entities/BasketItem.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace API.Entities
 {
+    // Anotations to add plural in the naming of the sql table
+    [Table("BasketItems")]
     public class BasketItem
     {
         public int Id { get; set; }
@@ -13,5 +16,8 @@ namespace API.Entities
         // navigation properties
         public int ProductId { get; set; }
         public Product Product { get; set; }
+        //relations to force cascade on delete with migrations creation
+        public int BasketId { get; set; }
+        public Basket Basket { get; set; }
     }
 }
