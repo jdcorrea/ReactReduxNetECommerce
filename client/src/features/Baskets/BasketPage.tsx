13a001fd7d2980bec10cda49db588361051c7598
Juan David Correa Cano <sye.jdcorrea@gmail.com>
2023-10-03 16:24:20 -0500

    Added Summary Component to basket

--- client/src/features/Baskets/BasketPage.tsx
+++ client/src/features/Baskets/BasketPage.tsx
@@ -1,10 +1,12 @@
 import Typography from '@mui/material/Typography'
-import { TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody, IconButton, Box } from "@mui/material"
+import { TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody, IconButton, Box, Grid } from "@mui/material"
 import { Add, Delete, Remove } from "@mui/icons-material"
 import { useStoreContext } from "@app/context/StoreContext"
 import agent from '@app/api/agent';
 import { useState } from 'react';
 import { LoadingButton } from '@mui/lab';
+import BasketSummary from './BasketSummary';
+import { currencyFormat } from '@app/util/util';
 
 
 function BasketPage() {
@@ -33,61 +35,69 @@ function BasketPage() {
   if (!basket) return <Typography variant="h3">Your basket is empty</Typography>
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Product</TableCell>
-            <TableCell align="right">Price</TableCell>
-            <TableCell align="center">Quantity</TableCell>
-            <TableCell align="right">Subtotal</TableCell>
-            <TableCell align="right"></TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {basket.items.map((item) => (
-            <TableRow
-              key={item.productId}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                <Box display='flex' alignItems='center'>
-                  <img src={item.pictureUrl} alt={item.name}
-                    style={{height: 50, marginRight: 20}}/>
-                </Box>
-              </TableCell>
-              <TableCell align="right">
-                ${(item.price / 100).toFixed(2)}
-              </TableCell>
-              <TableCell align="center">
-                <LoadingButton loading={status.loading && status.name === `rem${item.productId}`}
-                  color='error'
-                  onClick={() => handleRemoveItem(item.productId, 1, `rem${item.productId}`)}
-                >
-                  <Remove/>
-                </LoadingButton>
-                  {item.quantity}
-                <LoadingButton loading={status.loading && status.name === `add${item.productId}`}
-                  color='success'
-                  onClick={() => handleAddItem(item.productId, `add${item.productId}`)}
-                >
-                  <Add />
-                </LoadingButton>  
-              </TableCell>
-              <TableCell align="right">${(item.price * item.quantity / 100).toFixed(2)}</TableCell>
-              <TableCell align="right">
-                <LoadingButton loading={status.loading && status.name === `del${item.productId}`}
-                  color='error'
-                  onClick={() => handleRemoveItem(item.productId, item.quantity, `del${item.productId}`)}
-                >
-                  <Delete />
-                </LoadingButton>
-              </TableCell>
+    <>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Product</TableCell>
+              <TableCell align="right">Price</TableCell>
+              <TableCell align="center">Quantity</TableCell>
+              <TableCell align="right">Subtotal</TableCell>
+              <TableCell align="right"></TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {basket.items.map((item) => (
+              <TableRow
+                key={item.productId}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  <Box display='flex' alignItems='center'>
+                    <img src={item.pictureUrl} alt={item.name}
+                      style={{height: 50, marginRight: 20}}/>
+                  </Box>
+                </TableCell>
+                <TableCell align="right">
+                  {currencyFormat(item.price)}
+                </TableCell>
+                <TableCell align="center">
+                  <LoadingButton loading={status.loading && status.name === `rem${item.productId}`}
+                    color='error'
+                    onClick={() => handleRemoveItem(item.productId, 1, `rem${item.productId}`)}
+                  >
+                    <Remove/>
+                  </LoadingButton>
+                    {item.quantity}
+                  <LoadingButton loading={status.loading && status.name === `add${item.productId}`}
+                    color='success'
+                    onClick={() => handleAddItem(item.productId, `add${item.productId}`)}
+                  >
+                    <Add />
+                  </LoadingButton>  
+                </TableCell>
+                <TableCell align="right">{currencyFormat(item.price * item.quantity)}</TableCell>
+                <TableCell align="right">
+                  <LoadingButton loading={status.loading && status.name === `del${item.productId}`}
+                    color='error'
+                    onClick={() => handleRemoveItem(item.productId, item.quantity, `del${item.productId}`)}
+                  >
+                    <Delete />
+                  </LoadingButton>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Grid container>
+        <Grid item xs={6} />
+        <Grid item xs={6}>
+          <BasketSummary />
+        </Grid>
+      </Grid>
+    </>
   )
 }
 
---
+++ client/src/features/Baskets/BasketSummary.tsx
@@ -0,0 +1,40 @@
+import { useStoreContext } from "@app/context/StoreContext";
+import { currencyFormat } from "@app/util/util";
+import { TableContainer, Paper, Table, TableBody, TableRow, TableCell } from "@mui/material";
+
+export default function BasketSummary() {
+    const FREE_DELIVERY_MINIMUM_VALUE = 10000;
+    const DELIVERY_FEE_VALUE = 500;
+
+    const { basket } = useStoreContext();
+    const subtotal = basket?.items.reduce((subtotal, item) => subtotal + (item.price * item.quantity), 0) ?? 0;
+    const deliveryFee = subtotal < FREE_DELIVERY_MINIMUM_VALUE ? DELIVERY_FEE_VALUE : 0 ?? 0;
+
+    return (
+        <>
+            <TableContainer component={Paper} variant={'outlined'}>
+                <Table>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell colSpan={2}>Subtotal</TableCell>
+                            <TableCell align="right">{currencyFormat(subtotal)}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell colSpan={2}>Delivery fee*</TableCell>
+                            <TableCell align="right">{currencyFormat(deliveryFee)}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell colSpan={2}>Total</TableCell>
+                            <TableCell align="right">{currencyFormat(subtotal + deliveryFee)}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>
+                                <span style={{fontStyle: 'italic'}}>*Orders over $100 qualify for free delivery</span>
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </>
+    )
+}
