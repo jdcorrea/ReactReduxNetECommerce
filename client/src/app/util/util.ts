23f259070f2f4d6fbe71b5e71eaf1331c6173581
Juan David Correa Cano <sye.jdcorrea@gmail.com>
2023-10-03 16:22:24 -0500

    Added currency format to utils

--- client/src/app/util/util.ts
+++ client/src/app/util/util.ts
@@ -2,3 +2,7 @@ export function getCookie(key: string) {
   const cookieKey = document.cookie.match("(^|;)\\s*" + key + "\\s*=\\s*([^;]+)");
   return cookieKey ? cookieKey.pop() : "";
 }
+
+export function currencyFormat(amount: number) {
+  return `$${(amount / 100).toFixed(2)}`
+}
