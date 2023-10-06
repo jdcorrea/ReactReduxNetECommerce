d774309ed264f010cbc1f26a05799df6202b20db
Juan David Correa Cano <sye.jdcorrea@gmail.com>
2023-10-06 14:16:22 -0500

    Added use of Async Reducer with react redux toolkit

--- client/src/features/Baskets/basketSlice.ts
+++ client/src/features/Baskets/basketSlice.ts
@@ -1,13 +1,27 @@
+import agent from '@app/api/agent';
 import { Basket } from "@app/models/basket"
-import { createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 
 interface BasketState {
   basket: Basket | null
+  status: string
 }
 
 const initialState: BasketState = {
-  basket: null
+  basket: null,
+  status: 'idle'
 }
+
+export const addBasketItemAsync = createAsyncThunk<Basket, { productId: number, quantity: number}>(
+  'basket/addBasketItemAsync',
+  async ({ productId, quantity}) => {
+    try {
+      return await agent.Basket.addItem(productId, quantity)
+    } catch (error) {
+      console.log(error, 'Teets');
+    }
+  }
+)
 
 export const basketSlice = createSlice({
   name: 'basket',
@@ -21,10 +35,23 @@ export const basketSlice = createSlice({
       const itemIndex = state.basket?.items.findIndex(i => i.productId === productId)
       if (itemIndex === -1 || itemIndex === undefined) return
       state.basket!.items[itemIndex].quantity -= quantity
-      if (state.basket?.items[itemIndex].quantity <= 0)
-        state.basket.items.splice(itemIndex, 1)
+      if (state.basket!.items[itemIndex].quantity <= 0)
+        state.basket!.items.splice(itemIndex, 1)
     }
-  }
+  },
+  extraReducers: (builder => {
+      builder.addCase(addBasketItemAsync.pending, (state) => {
+        state.status = 'pendingAddItem'
+      })
+      builder.addCase(addBasketItemAsync.fulfilled, (state, action) => {
+        state.basket = action.payload
+        state.status = 'idle'
+      })
+      builder.addCase(addBasketItemAsync.rejected, (state) => {
+        state.status = 'idle'
+      })
+  })
 })
+
 
 export const { setBasket, removeItem } = basketSlice.actions
