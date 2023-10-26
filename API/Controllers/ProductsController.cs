e049749a1ed6ecf57cb9b546f96331b1bb6f30a0
Juan David Correa Cano <sye.jdcorrea@gmail.com>
2023-10-26 07:25:17 -0500

    Added search functionality to Products fetch

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -16,10 +16,14 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts(string orderBy)
+        public async Task<ActionResult<List<Product>>> GetProducts(
+            string orderBy,
+            string searchTerm
+        )
         {
             var query =  _context.Products
             .Sort(orderBy)
+            .Search(searchTerm)
             .AsQueryable();
 
             return await query.ToListAsync();
