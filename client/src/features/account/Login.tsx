7fc4489d37d6643533bbb7b7ad4a08b26252e207
Juan David Correa Cano <sye.jdcorrea@gmail.com>
2023-11-28 15:35:37 -0500

    Added useForm in login form

--- client/src/features/account/Login.tsx
+++ client/src/features/account/Login.tsx
@@ -1,6 +1,4 @@
-import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
@@ -9,22 +7,23 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { Paper } from '@mui/material';
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
 import agent from '@app/api/agent';
+import { FieldValues, useForm } from 'react-hook-form';
+import { LoadingButton } from '@mui/lab';
 
 export default function Login() {
-  const [values, setValues] = useState({
-    username: '',
-    password: ''
+  const {register, handleSubmit, formState: {
+    isSubmitting, errors, isValid
+  }} = useForm({
+    mode: 'onTouched'
   });
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    agent.Account.login(values);
-  };
 
-  function handleInputChange(event: any) {
-    const {name, value} = event.target;
-    setValues({...values, [name]: value})
+  async function submitForm(data: FieldValues) {
+    try {
+      await agent.Account.login(data);
+    } catch (error) {
+      
+    }
   }
 
   return (
@@ -44,34 +43,35 @@ export default function Login() {
       <Typography component="h1" variant="h5">
         Sign in
       </Typography>
-      <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+      <Box component="form" onSubmit={handleSubmit(submitForm)} noValidate sx={{ mt: 1 }}>
         <TextField
           margin="normal"
           fullWidth
           label="Username"
-          name="username"
           autoFocus
-          onChange={handleInputChange}
-          value={values.username}
+          {...register('username', {required: 'Username is required'})}
+          error={!!errors.username}
+          helperText={errors?.username?.message as string}
         />
         <TextField
           margin="normal"
           fullWidth
-          name="password"
           label="Password"
           type="password"
-          autoComplete="current-password"
-          onChange={handleInputChange}
-          value={values.password}
+          {...register('password', {required: 'Password is required'})}
+          error={!!errors.password}
+          helperText={errors?.password?.message as string}
         />
-        <Button
+        <LoadingButton
+          loading={isSubmitting}
+          disabled={!isValid}
           type="submit"
           fullWidth
           variant="contained"
           sx={{ mt: 3, mb: 2 }}
         >
           Sign In
-        </Button>
+        </LoadingButton>
         <Grid container>
           <Grid item>
             <Link to="/register">
